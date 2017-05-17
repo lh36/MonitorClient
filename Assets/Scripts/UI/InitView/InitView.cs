31cb362161cba13cdbd40e9c328aa0f038bbcc00
heyiwen <heyiwen555@163.com>
2017-05-17 20:23:04 +0800

    commit

--- Assets/Scripts/UI/InitView/InitView.cs
+++ Assets/Scripts/UI/InitView/InitView.cs
@@ -7,12 +7,14 @@ public class InitView : MonoBehaviour
     private InitModel m_Model;
 
     private Button Btn_Start;
+    private Button Btn_Quit;
 
     void Awake()
     {
         this.m_Model = new InitModel ();
 
         Btn_Start = transform.Find ("Btn_Start").gameObject.GetComponent<Button> ();
+        Btn_Quit = transform.Find ("Btn_Quit").gameObject.GetComponent<Button> ();
     }
 
     void Start()
@@ -21,11 +23,19 @@ public class InitView : MonoBehaviour
         Btn_Start.onClick.AddListener (delegate {
             OnStartClick ();
         });
+        Btn_Quit.onClick.AddListener (delegate {
+            OnQuitClick ();
+        });
     }
 
     private void OnStartClick()
     {
         this.m_Model.OnStartClicked ();
+    }
+
+    private void OnQuitClick()
+    {
+        Application.Quit ();
     }
 
 }
